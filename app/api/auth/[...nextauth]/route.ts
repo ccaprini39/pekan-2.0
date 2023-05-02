895ff1aa4ccfd15266d13b82a2b72760efd0eae8
Collin Caprini <collin.caprini@agile5technologies.com>
2023-05-02 09:26:01 -0400

    type error

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -10,8 +10,8 @@ export const authOptions: NextAuthOptions = {
     adapter: XataAdapter(client),
     providers: [
         GoogleProvider({
-            clientId: process.env.GOOGLE_ID,
-            clientSecret: process.env.GOOGLE_SECRET,
+            clientId: process.env.GOOGLE_ID || "",
+            clientSecret: process.env.GOOGLE_SECRET || "",
         }),
         // ...add more providers here
     ],
